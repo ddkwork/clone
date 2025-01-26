b3b821b4b6bb642c100daff41c90dbefe0019fc8
Admin <2762713521@qq.com>
2025-01-26 18:33:27 +0800

    dev

---
+++ gen.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/ddkwork/golibrary/safemap"
+	"github.com/ddkwork/golibrary/stream"
+)
+
+func main() {
+	m := safemap.NewOrdered[string, string](func(yield func(string, string) bool) {
+		yield("miqt", "https://github.com/ddkwork/miqt.git")
+		yield("HyperDbg", "https://github.com/HyperDbg/HyperDbg.git")
+	})
+	name := "HyperDbg"
+	url := m.GetMust(name)
+	g := stream.NewGeneratedFile()
+	g.P(`
+name: clone仓库
+
+on:
+  push:
+    branches:
+      - master  # 或者你想要触发工作流的分支名
+  workflow_dispatch:
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: 检出代码
+        uses: actions/checkout@v4
+`)
+	g.P("      - name: 克隆", name, "仓库")
+	g.P("        run: git clone --recursive ", url)
+	g.P("      - name: 打包", name, "项目")
+	g.P("        run: zip -r "+name, ".zip ", name)
+	g.P()
+	g.P("      - name: 上传打包文件")
+	g.P("        uses: actions/upload-artifact@v4")
+	g.P("        with:")
+	g.P("          name: ", name, "-zip")
+	g.P("          path: ", name, ".zip")
+	stream.WriteTruncate(".github/workflows/clone.yml", g.String())
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module clone
+
+go 1.23.3
+
+require github.com/ddkwork/golibrary v0.0.0-20250101093329-d96f6524ebe4
+
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/dc0d/caseconv v0.5.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.28.0 // indirect
+	mvdan.cc/gofumpt v0.7.0 // indirect
+)
