ae5eca849a2520ad8d2f6742b753a2b1b4290672
Admin <2762713521@qq.com>
2025-03-05 21:51:37 +0800

    dev

--- gen.go
+++ gen.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/safemap"
 	"github.com/ddkwork/golibrary/stream"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -40,47 +37,15 @@ jobs:
 	g.P("      - name: 打包", name, "项目")
 	g.P("        run: tar -czvf "+name, ".tar.gz ", name)
 	g.P()
+
+	g.P("      - name: Run tests")
+	g.P("        run: go test .")
+	g.P()
+
 	g.P("      - name: 上传打包文件")
 	g.P("        uses: actions/upload-artifact@v4")
 	g.P("        with:")
 	g.P("          name: ", name, "-tar-gz")
 	g.P("          path: ", name, ".tar.gz")
 	stream.WriteTruncate(".github/workflows/clone.yml", g.String())
-}
-
-func UpdateDependencies() { //模块代理刷新的不及时，需要禁用代理
-	mylog.Check(os.Setenv("GOPROXY", "direct"))
-	for s := range strings.Lines(`
-     go get -x gioui.org@main
-	 go get -x gioui.org/cmd@main
-	 go get -x gioui.org/example@main
-	 go get -x gioui.org/x@main
-	 go get -x github.com/oligo/gvcode@main
-	 go get -x github.com/ddkwork/golibrary@master
-	 go get -x github.com/ddkwork/ux@master
-	 go get -x github.com/google/go-cmp@master
-	 go get -x github.com/ddkwork/app@master
-	 go get -x github.com/ddkwork/toolbox@master
-	 go get -x github.com/ddkwork/unison@master
-	 go get -x github.com/ebitengine/purego@main
-	 go get -x github.com/saferwall/pe@main
-	 ::go get -u -x all
-	 //go mod tidy
-
-	//go install mvdan.cc/gofumpt@latest
-	//gofumpt -l -w .
-	//go install honnef.co/go/tools/cmd/staticcheck@latest
-	//staticcheck ./...
-	//go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
-
-`) {
-		s = strings.TrimSpace(s)
-		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
-			continue
-		}
-		stream.RunCommand(s)
-	}
-	for s := range stream.ReadFileToLines("go.mod") {
-		println(s)
-	}
 }
---
+++ gen_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/ddkwork/golibrary/mylog"
+	"github.com/ddkwork/golibrary/stream"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	UpdateDependencies()
+}
+
+func UpdateDependencies() { //模块代理刷新的不及时，需要禁用代理
+	mylog.Check(os.Setenv("GOPROXY", "direct"))
+	for s := range strings.Lines(`
+     go get -x gioui.org@main
+	 go get -x gioui.org/cmd@main
+	 go get -x gioui.org/example@main
+	 go get -x gioui.org/x@main
+	 go get -x github.com/oligo/gvcode@main
+	 go get -x github.com/ddkwork/golibrary@master
+	 go get -x github.com/ddkwork/ux@master
+	 go get -x github.com/google/go-cmp@master
+	 go get -x github.com/ddkwork/app@master
+	 go get -x github.com/ddkwork/toolbox@master
+	 go get -x github.com/ddkwork/unison@master
+	 go get -x github.com/ebitengine/purego@main
+	 go get -x github.com/saferwall/pe@main
+	 ::go get -u -x all
+	 //go mod tidy
+
+	//go install mvdan.cc/gofumpt@latest
+	//gofumpt -l -w .
+	//go install honnef.co/go/tools/cmd/staticcheck@latest
+	//staticcheck ./...
+	//go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
+
+`) {
+		s = strings.TrimSpace(s)
+		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
+			continue
+		}
+		stream.RunCommand(s)
+	}
+	for s := range stream.ReadFileToLines("go.mod") {
+		println(s)
+	}
+}
