f41e5ee393c784f6a048b7cac9419964a3cfa412
Admin <2762713521@qq.com>
2025-03-05 22:52:16 +0800

    dev

--- gen.go
+++ gen.go
@@ -63,7 +63,7 @@ jobs:
           sudo apt install gcc pkg-config libwayland-dev libx11-dev libx11-xcb-dev libxkbcommon-x11-dev libgles2-mesa-dev libegl1-mesa-dev libffi-dev libxcursor-dev libvulkan-dev
 
       - name: Run tests
-        run: go test .
+        run: go test -v .
 
 `)
 
