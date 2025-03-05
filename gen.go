f277c300a7d2bf87f8115749b12e8407c2035993
Admin <2762713521@qq.com>
2025-03-05 21:47:01 +0800

    dev

--- gen.go
+++ gen.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/safemap"
 	"github.com/ddkwork/golibrary/stream"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -11,7 +14,6 @@ func main() {
 		yield("HyperDbg", "https://github.com/HyperDbg/HyperDbg.git")
 		yield("sointu", "https://github.com/vsariola/sointu.git")
 		yield("unison", "https://github.com/richardwilkes/unison.git")
-		yield("unison", "https://github.com/go-gost/gost-plus.git")
 	})
 	name := "unison"
 	url := m.GetMust(name)
@@ -44,4 +46,41 @@ jobs:
 	g.P("          name: ", name, "-tar-gz")
 	g.P("          path: ", name, ".tar.gz")
 	stream.WriteTruncate(".github/workflows/clone.yml", g.String())
+}
+
+func UpdateDependencies() { //模块代理刷新的不及时，需要禁用代理
+	mylog.Check(os.Setenv("GOPROXY", "direct"))
+	for s := range strings.Lines(`
+     go get -x gioui.org@main
+	 go get -x gioui.org/cmd@main
+	 go get -x gioui.org/example@main
+	 go get -x gioui.org/x@main
+	 go get -x github.com/oligo/gvcode@main
+	 go get -x github.com/ddkwork/golibrary@master
+	 go get -x github.com/ddkwork/ux@master
+	 go get -x github.com/google/go-cmp@master
+	 go get -x github.com/ddkwork/app@master
+	 go get -x github.com/ddkwork/toolbox@master
+	 go get -x github.com/ddkwork/unison@master
+	 go get -x github.com/ebitengine/purego@main
+	 go get -x github.com/saferwall/pe@main
+	 ::go get -u -x all
+	 //go mod tidy
+
+	//go install mvdan.cc/gofumpt@latest
+	//gofumpt -l -w .
+	//go install honnef.co/go/tools/cmd/staticcheck@latest
+	//staticcheck ./...
+	//go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
+
+`) {
+		s = strings.TrimSpace(s)
+		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
+			continue
+		}
+		stream.RunCommand(s)
+	}
+	for s := range stream.ReadFileToLines("go.mod") {
+		println(s)
+	}
 }
