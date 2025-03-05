a3e17e3308904b6f36d25da8375a83e79bf41796
Admin <2762713521@qq.com>
2025-03-05 22:05:54 +0800

    dev

--- gen.go
+++ gen.go
@@ -38,9 +38,30 @@ jobs:
 	g.P("        run: tar -czvf "+name, ".tar.gz ", name)
 	g.P()
 
-	g.P("      - name: Run tests")
-	g.P("        run: go test .")
-	g.P()
+	g.PKeepSpace(`      
+      - name: Setup Go
+        uses: actions/setup-go@v4
+        with:
+          go-version: '1.24.0'
+
+      # manjaro linux
+      # gioui提示vulkan构建约束被排除的原因是没有安装gcc，termux可以试试是不是这个原因
+      # sudo pacman -S pkg-config vulkan-headers gcc clang cmake
+      # yay -S android-sdk-build-tools  android-sdk-platform-tools
+      # yay -S android-platform
+      # go env -w GOPROXY=https://goproxy.cn,direct
+      # go run 需要sudo
+
+      - name: Install C library dependencies on Ubuntu
+        if: matrix.os == 'ubuntu-latest'
+        run: |
+          sudo apt-get update
+          sudo apt install gcc pkg-config libwayland-dev libx11-dev libx11-xcb-dev libxkbcommon-x11-dev libgles2-mesa-dev libegl1-mesa-dev libffi-dev libxcursor-dev libvulkan-dev
+
+      - name: Run tests
+        run: go test .
+
+`)
 
 	g.P("      - name: 上传打包文件")
 	g.P("        uses: actions/upload-artifact@v4")
