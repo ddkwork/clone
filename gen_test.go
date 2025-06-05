1d90958c7864e93f9b3ac2e8b20dec1673384c2b
Admin <2762713521@qq.com>
2025-06-05 23:33:21 +0800

    dev

--- gen_test.go
+++ gen_test.go
@@ -14,33 +14,35 @@ func TestUpdateAllLocalRep(t *testing.T) {
 }
 func TestName(t *testing.T) { // 模块代理刷新的不及时，需要禁用代理
 	mylog.Check(os.Setenv("GOPROXY", "direct"))
-	for s := range strings.Lines(`
-     go get -x gioui.org@main
-	 go get -x gioui.org/cmd@main
-	 go get -x gioui.org/example@main
-	 go get -x gioui.org/x@main
-	 go get -x github.com/oligo/gvcode@main
-	 go get -x github.com/ddkwork/golibrary@master
-	 go get -x github.com/ddkwork/ux@master
-	 go get -x github.com/ddkwork/ddk@master
-	 go get -x github.com/ddkwork/bindgen@master
-	 go get -x github.com/ddkwork/crypt@master
-	 go get -x github.com/google/go-cmp@master
-	 go get -x golang.org/x/arch@master
-	 go get -x modernc.org/ccgo@master
-	 go get -x golang.org/x/tools/gopls@master
-	 go get -x github.com/ddkwork/app@master
-	 go get -x github.com/ddkwork/toolbox@master
-	 go get -x github.com/ddkwork/unison@master
-	 go get -x github.com/ebitengine/purego@main
-	 go get -x github.com/saferwall/pe@main
-`) {
-		s = strings.TrimSpace(s)
-		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
-			continue
-		}
-		stream.RunCommand(s)
+	reps := map[string]string{
+		"gioui.org":                    "main",
+		"gioui.org/cmd":                "main",
+		"gioui.org/example":            "main",
+		"gioui.org/x":                  "main",
+		"github.com/oligo/gvcode":      "main",
+		"github.com/ddkwork/golibrary": "master",
+		"github.com/ddkwork/ux":        "master",
+		"github.com/ddkwork/ddk":       "master",
+		"github.com/ddkwork/bindgen":   "master",
+		"github.com/ddkwork/crypt":     "master",
+		"github.com/google/go-cmp":     "master",
+		"golang.org/x/arch":            "master",
+		"modernc.org/ccgo":             "master",
+		"golang.org/x/tools/gopls":     "master",
+		"github.com/ddkwork/app":       "master",
+		"github.com/ddkwork/toolbox":   "master",
+		"github.com/ddkwork/unison":    "master",
+		"github.com/ebitengine/purego": "main",
+		"github.com/saferwall/pe":      "main",
 	}
+	var all []string
+	for k, v := range reps {
+		if strings.Contains(k, "gvcode") {
+			v = "v0.2.1-0.20250424030509-8138ffc92f73"
+		}
+		all = append(all, k+"@"+v)
+	}
+	stream.RunCommand("go", "get", strings.Join(all, " "))
 	g := stream.NewGeneratedFile()
 	for s := range stream.ReadFileToLines("go.mod") {
 		if strings.Contains(s, "gvcode") {
@@ -50,5 +52,4 @@ func TestName(t *testing.T) { // 模块代理刷新的不及时，需要禁用�
 	}
 	stream.WriteTruncate("go.mod", g.Bytes())
 	mylog.Json("mod", string(mylog.Check2(os.ReadFile("go.mod"))))
-	mylog.Json("log", string(mylog.Check2(os.ReadFile("log.log"))))
 }
